adbbf521eb7c7b015dafa9c243adeae36b734077
Brian Whitman <brian@echonest.com>
2016-04-17 08:20:25 -0400

    copying bread detector over

---
+++ auth.h
@@ -0,0 +1,11 @@
+
+const char WiFiSSID[] = "peony";
+const char WiFiPSK[] = "brianskehan";
+const int LED_PIN = 5; // Thing's onboard, green LED
+const int ANALOG_PIN = A0; // The only analog pin on the Thing
+const int DIGITAL_PIN = 12; // Digital pin to be read
+const char PhantHost[] = "data.sparkfun.com";
+const char PublicKey[] = "bGjMlzNxlwF0nJzKZAaJ";
+const char PrivateKey[] = "VpRkoAYxozCJWwK4G7qw";
+// deleteKey 5oKVp21ypLSrXp5k3dQp
+
